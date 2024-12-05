0572e008445f7758581397f0e772ec8e71c723f7
정회석 <jeonghoe@jeonghoeseog-ui-noteubug.local>
2024-12-05 14:14:15 +0900

    test

--- test_cicd/src/test/java/com/nhnacademy/test_cicd/TestCicdApplicationTests.java
+++ test_cicd/src/test/java/com/nhnacademy/test_cicd/TestCicdApplicationTests.java
@@ -1,13 +1,21 @@
 package com.nhnacademy.test_cicd;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TestCicdApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		// 애플리케이션 컨텍스트가 로드되었는지 확인
+		assertThat(applicationContext).isNotNull();
 	}
-
 }
