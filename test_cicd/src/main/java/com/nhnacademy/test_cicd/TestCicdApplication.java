d806e3a26cd8bb5e9113543b7887145c17aeb4b9
정회석 <jeonghoe@jeonghoeseog-ui-noteubug.local>
2024-12-05 14:25:24 +0900

    chane2

--- test_cicd/src/main/java/com/nhnacademy/test_cicd/TestCicdApplication.java
+++ test_cicd/src/main/java/com/nhnacademy/test_cicd/TestCicdApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestCicdApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(TestCicdApplication.class, args);
 	}
